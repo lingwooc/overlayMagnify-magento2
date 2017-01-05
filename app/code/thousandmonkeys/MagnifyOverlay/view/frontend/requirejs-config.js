e86b916ce021f4cafb8358d9b3dd9de397634cee
Chris Lingwood <chris@thousandmonkeys.com>
2017-01-05 14:44:29 +0000

    created

---
+++ app/code/thousandmonkeys/MagnifyOverlay/view/frontend/requirejs-config.js
@@ -0,0 +1,12 @@
+var config = {
+    "paths": {
+        "magnifier/magnify": "thousandmonkeys_OverlayMagnify/js/magnify",
+    },
+    'config': {
+        'mixins': {
+            'mage/gallery/gallery': {
+                "thousandmonkeys_OverlayMagnify/js/galleryMixin": true
+            }
+        }
+    }
+};
